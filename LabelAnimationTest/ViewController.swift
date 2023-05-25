ee5c154b47c500a9a21da5a474b3c5258d580463
Mashqur Habib Himel <mashqur.himel@tigerit.com>
2023-05-25 10:27:18 +0600

    Initial Commit

---
+++ LabelAnimationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabelAnimationTest
+//
+//  Created by Mashqur Habib Himel on 25/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
